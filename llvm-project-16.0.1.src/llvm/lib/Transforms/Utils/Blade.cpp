cfa070bc00fecb62456ec6aa8745821cafd07174
maxgallup <m.gallup@student.vu.nl>
2023-05-21 18:19:18 +0200

    leakyPahts vector now contains correct paths

--- llvm-project-16.0.1.src/llvm/lib/Transforms/Utils/Blade.cpp
+++ llvm-project-16.0.1.src/llvm/lib/Transforms/Utils/Blade.cpp
@@ -26,11 +26,11 @@ class BladeNode {
   public:
     Instruction* I;
     BladeNode* parent;
-    SmallVector<BladeNode*, 16> *nodes;
+    SmallVector<BladeNode*, 16> *children;
     BladeNode(Instruction *newI, BladeNode *newParent) {
       I = newI;
       parent = newParent;
-      nodes = new SmallVector<BladeNode*, 16>();
+      children = new SmallVector<BladeNode*, 16>();
     };
 };
 
@@ -164,89 +164,96 @@ void identifyLeak(Instruction *I, SmallVector<SmallVector<Instruction*, 16>, 16>
 
 
 
-void somethingRecursive(Instruction *I) {  
 
-  for (User *U : I->users()) {
-    if (Instruction *II = dyn_cast<Instruction>(U)) {
-      if (isStableInstruction(II)) {
-        
-        return;
-      }
-      
-      // somethingRecursive(II);
-    }
-  }
-  return;
-}
-
-
-void identifyLeak2(Instruction *I, SmallVector<SmallVector<Instruction*, 16>, 16> *leakypaths) {
-
-
-  if (isTransientInstruction(I)) {
-    D("Start: " << *I);
-    somethingRecursive(I);
-  }
-
-}
-
-
-// Walk through the entire graph and free all nodes
+/// @brief Traverse graph in Post-Order and delete (free) all nodes to prevent a memory leak.
+/// @param B Root node of graph.
 void freeBladeNodes(BladeNode *B) {
-  if (B->nodes->empty()) {
-    D("freeing: " << *B->I);
-    delete(B->nodes);
+  if (B->children->empty()) {
+    delete(B->children);
     delete(B);
     return;
   }
 
-  for (BladeNode *node : *B->nodes) {
+  for (BladeNode *node : *B->children) {
     freeBladeNodes(node);
   }
 
+  delete(B->children);
+  delete(B);
+  return;
 }
 
-
-void findStableRec(Instruction *I, BladeNode *parent, SmallVector<BladeNode*, 16> *si) {
+/// @brief Builds the DAG of BladeNodes that make up all Def-Use chains throughout the program,
+/// we then save the stable leaf BladeNodes into a vector so that we can construct the correct
+/// def-use chain going bottom up.
+/// @param I Instruction whose users will turn into children.
+/// @param parent Pointer to parent instruction, starting transient instruction has NULL.
+/// @param stable_insts Collection of stable instruction leaf nodes that we are interested in.
+void findStableRec(Instruction *I, BladeNode *parent, SmallVector<BladeNode*, 16> *stable_insts) {
   for (User *U : I->users()) {
     if (Instruction *II = dyn_cast<Instruction>(U)) {
       auto user = new BladeNode(II, parent);
-      parent->nodes->push_back(user);
+      parent->children->push_back(user);
 
       if (isStableInstruction(II)) {
-        si->push_back(user);
+        stable_insts->push_back(user);
       }
 
-      findStableRec(II, user, si);
+      findStableRec(II, user, stable_insts);
     }
   }
 }
 
 
-void identifyLeak3(Instruction *I, SmallVector<SmallVector<Instruction*, 16>, 16> *leakypaths) {
 
+/// @brief Walks through the Def-Use chain of the given instruction to identify all paths
+/// from T->S. A path is given by a vector of instructions from stable instruction to
+/// transient. These vectors are then collected into a 2D vector.
+/// @param I The transient instruction to start looking from.
+/// @param leaky_paths Collection of all secret leaking paths.
+void gatherLeaks(Instruction *I, SmallVector<SmallVector<Instruction*, 16>, 16> *leaky_paths) {
+
+  // Since a leak can only come from a transient instruction, we can ignore all others
   if (isTransientInstruction(I)) {
+
+    // For this instruction we will save all paths (made up via BladeNodes) that end
+    // in a stable instruction.
     auto stable_instructions = SmallVector<BladeNode*, 16>();
     auto start = new BladeNode(I, NULL);
     findStableRec(I, start, &stable_instructions);
 
+    // Since we store the parent of each BladeNode, we can traverse the graph bottom 
+    // up from the stable instruction to the start, leaving us with only those 
+    // instructions directly part of the def-use chain.
     for (BladeNode *B : stable_instructions) {
-      D("Stable Inst: " << *B->I);
-      // auto a_leaky_path = SmallVector<Instruction*, 16>();
-      // BladeNode* current = B;
-      // while (current->parent) {
-      //   a_leaky_path.push_back(current->I);
-      //   current = current->parent;
-      // }
-      // leakypaths->push_back(a_leaky_path);
+      auto a_leaky_path = SmallVector<Instruction*, 16>();
+      BladeNode* current = B;
+      while (current->parent) {
+        a_leaky_path.push_back(current->I);
+        current = current->parent;
+      }
+      a_leaky_path.push_back(I);
+      leaky_paths->push_back(a_leaky_path);
     }
 
+    // Since we've accumulated all BladeNodes on the stack and we've alread saved the leaky path
+    // of instructions, we must free all nodes in Post-Order traversal.
     freeBladeNodes(start);
 
   }
 }
 
-
+int print_leaky_paths(SmallVector<SmallVector<Instruction*, 16>, 16> *leaky_paths) {
+  int count = 0;
+  for (SmallVector<Instruction*, 16> S : *leaky_paths) {
+    count++;
+    S("\tLeak " << count);
+    for (Instruction* I : S) {
+      D("\t\t" << *I);
+    }
+  }
+  return count;
+}
 
 PreservedAnalyses BladePass::run(Module &M, ModuleAnalysisManager &AM) {
   // First pass to add marks 
@@ -259,30 +266,23 @@ PreservedAnalyses BladePass::run(Module &M, ModuleAnalysisManager &AM) {
     }
   }
 
-  auto leakyPaths = SmallVector<SmallVector<Instruction*, 16>, 16>();
-
-  // Second pass to identify Transient -> Stable paths and isolate
+  auto leaky_paths = SmallVector<SmallVector<Instruction*, 16>, 16>();
+  // Second pass to collect all secret leaking paths into leaky_paths
   for (Function &F : M) {
     for (BasicBlock &BB : F) {
       for (Instruction &I : BB) {
-        // identifyLeak(&I, &leakyPaths);
-        // identifyLeak2(&I, &leakyPaths);
-        identifyLeak3(&I, &leakyPaths);
+        // identifyLeak(&I, &leaky_paths);
+        // identifyLeak2(&I, &leaky_paths);
+        gatherLeaks(&I, &leaky_paths);
       }
     }
   }
 
+  int total_leaky_paths = print_leaky_paths(&leaky_paths);
 
-  auto count = 0;
-  for (SmallVector<Instruction*, 16> S : leakyPaths) {
-    count++;
-    D("\tLeak " << count);
-    for (Instruction* I : S) {
-      D("\t\t" << *I);
-    }
-  }
-
-  S("DONE, Total leaks: " << count);
+  S("------------------------");
+  S("  DONE, Total leaks: " << total_leaky_paths);
+  S("------------------------");
 
   return PreservedAnalyses::all();
 }
--- test/csrc/optimal.c
+++ test/csrc/optimal.c
@@ -8,15 +8,9 @@ int main() {
 	srand(time(0));
 	int a[5] = {0,1,2,3,4};
 	int i = rand() % 5;
-	
-
-
 	int y = i + 3;
 	int x = i + 4;
-
-
 	int res = a[y];
 	int res2 = a[x];
-	
 	return res + res2;
 }
